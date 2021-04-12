5c3c00d7f0c03ca46406b8b35ed83b9b581d107b
Matheus Assmann <matheusassmann@hotmail.com>
2021-04-12 10:49:24 -0300

    Gettes/Setters/Constructor

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,8 +1,32 @@
+import {Cliente} from "./Cliente.js"
+
 export class ContaCorrente{
+    static numeroDeContas = 0;
     agencia;
-    cliente;
+    _cliente;
     // #saldo (private) = https://github.com/tc39/proposal-class-fields#private-fields
     _saldo = 0;
+
+    constructor(cliente, agencia){
+        this.cliente = cliente;
+        this.agencia = agencia;
+        ContaCorrente.numeroDeContas += 1;
+    }
+
+    set cliente(novoValor){
+        if (novoValor instanceof Cliente){
+            this._cliente = novoValor;
+        }
+    }
+
+    get cliente(){
+        return this._cliente;
+    }
+
+    get saldo(){
+        return this._saldo;
+    }
+
     sacar(valor){
         if (this._saldo >= valor){
             this._saldo -= valor;
--- index.js
+++ index.js
@@ -1,25 +1,16 @@
 import {Cliente} from "./Cliente.js"
 import {ContaCorrente} from "./ContaCorrente.js"
 
-const cliente1 = new Cliente();
-cliente1.nome = "Ricardo";
-cliente1.cpf = 11122233309;
+const cliente1 = new Cliente("Ricardo", 11122233309);
+const cliente2 = new Cliente("Alice", 88822233309);
 
-const cliente2 = new Cliente();
-cliente2.nome = "Alice";
-cliente2.cpf = 88822233309;
-
-const contaCorrenteRicardo = new ContaCorrente();
-contaCorrenteRicardo.agencia = 1001;
-contaCorrenteRicardo.cliente = cliente1;
+const contaCorrenteRicardo = new ContaCorrente(cliente1, 1001);
 contaCorrenteRicardo.depositar(500);
 
-const conta2 = new ContaCorrente();
-conta2.cliente = cliente2;
-conta2.agencia = 102;
+const conta2 = new ContaCorrente(cliente2, 102);
 
 let valor = 200;
 contaCorrenteRicardo.tranferir(valor, conta2);
 
 console.log("valor: ", valor)
-console.log(conta2);
+console.log(ContaCorrente.numeroDeContas);
